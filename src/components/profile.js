bfd6e1e345a1b8e8b205e9e38aa59e756c10fbdb
Gabriela Villarreal <gabrielaavd@gmail.com>
2022-11-16 11:02:18 -0300

    gif profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -2,8 +2,8 @@ export const viewForProfile = ()  => {
     
     const profileDiv = document.createElement('div');
 
-    profileDiv.textContent = 'Profile'
-
+    profileDiv.innerHTML = `<iframe src="https://giphy.com/embed/1Zbeweu52ZaQE" width="480" height="402" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/tumbleweed-1Zbeweu52ZaQE">via GIPHY</a></p>`
+    
     return profileDiv
 }
 
